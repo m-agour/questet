07bc2287acad7f7cc0d029e13771a1b6c19d1662
m-agour <mo.aggour@gmail.com>
2022-03-18 23:51:26 +0200

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,25 +1,87 @@
 const Sequelize = require("sequelize");
-
-module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define(
-        "user", // Model name
-        {
-            // Attributes
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define(
+        "user", {
             id: {
-                type: DataTypes.INTEGER,
                 autoIncrement: true,
+                type: DataTypes.INTEGER,
+                allowNull: false,
                 primaryKey: true,
             },
-            name: {
-                type: DataTypes.STRING,
+            firstName: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            middleName: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            lastName: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            DOB: {
+                type: DataTypes.DATEONLY,
+                allowNull: true,
+            },
+            email: {
+                type: DataTypes.STRING(100),
+                allowNull: true,
+            },
+            pass: {
+                type: DataTypes.STRING(1024),
+                allowNull: true,
+            },
+            gender: {
+                type: DataTypes.CHAR(1),
+                allowNull: true,
+            },
+            currentPosition: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            activated: {
+                type: DataTypes.TINYINT,
+                allowNull: true,
+            },
+            google: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            facebock: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            country: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            city: {
+                type: DataTypes.STRING(45),
+                allowNull: true,
+            },
+            timeZone: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            isOnline: {
+                type: DataTypes.TINYINT,
+                allowNull: true,
+            },
+            lastOnline: {
+                type: DataTypes.DATEONLY,
+                allowNull: true,
             },
         }, {
-            // Options
-            timestamps: true,
-            underscrored: true,
-            createdAt: "created_at",
-            updatedAt: "updated_at",
+            sequelize,
+            tableName: "user",
+            timestamps: false,
+            indexes: [{
+                name: "PRIMARY",
+                unique: true,
+                using: "BTREE",
+                fields: [{ name: "id" }],
+            }, ],
         }
     );
-    return User;
 };
