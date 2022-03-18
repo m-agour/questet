89dde7498fe522479c9c00fd4487e616459b7cb6
m-agour <mo.aggour@gmail.com>
2022-03-18 23:58:19 +0200

    Update question.js

---
+++ backend/models/question.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define(
+        "question", {
+            id: {
+                autoIncrement: true,
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+            },
+            questionType: {
+                type: DataTypes.CHAR(1),
+                allowNull: true,
+            },
+            questionData: {
+                type: DataTypes.STRING(5000),
+                allowNull: true,
+            },
+            examId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "exam",
+                    key: "id",
+                },
+            },
+        }, {
+            sequelize,
+            tableName: "question",
+            timestamps: false,
+            indexes: [{
+                    name: "PRIMARY",
+                    unique: true,
+                    using: "BTREE",
+                    fields: [{ name: "id" }],
+                },
+                {
+                    name: "question_exam1_fk_idx",
+                    using: "BTREE",
+                    fields: [{ name: "examId" }],
+                },
+            ],
+        }
+    );
+};
