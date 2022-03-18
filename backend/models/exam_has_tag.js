ccb23654d0070a04e8fbda344f40d29097dd9373
m-agour <mo.aggour@gmail.com>
2022-03-18 23:59:57 +0200

    Create exam_has_tag.js

---
+++ backend/models/exam_has_tag.js
@@ -0,0 +1,38 @@
+const Sequelize = require("sequelize");
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define(
+        "exam_has_tag", {
+            examId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "exam",
+                    key: "id",
+                },
+            },
+            tagTitle: {
+                type: DataTypes.STRING(45),
+                allowNull: false,
+                references: {
+                    model: "tag",
+                    key: "title",
+                },
+            },
+        }, {
+            sequelize,
+            tableName: "exam_has_tag",
+            timestamps: false,
+            indexes: [{
+                    name: "exam_has_tag_tag1_fk_idx",
+                    using: "BTREE",
+                    fields: [{ name: "tagTitle" }],
+                },
+                {
+                    name: "exam_has_tag_exam1_fk_idx",
+                    using: "BTREE",
+                    fields: [{ name: "examId" }],
+                },
+            ],
+        }
+    );
+};
