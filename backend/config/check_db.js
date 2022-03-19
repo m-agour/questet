57a6621799837b35135c93a6f5e7b54e81c761a7
m-agour <mo.aggour@gmail.com>
2022-03-19 23:09:41 +0200

    edited db

---
+++ backend/config/check_db.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "",
+});
+connection
+    .query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`)
+    .then(console.log("hello"));
