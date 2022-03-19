b8d1f358f2c6fb24390a6c607ad145dcb3462dd7
m-agour <mo.aggour@gmail.com>
2022-03-20 00:25:15 +0200

    Update user.js

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const { dirname } = require("path");
+
 const db = require(dirname(require.main.filename) + "/models");
+
 const router = express.Router();
 
 /* GET home page. */
 router.get("/testing", (req, res) => {});
+
+router.get("/getUsers", async(req, res) => {
+    let data = await db.user.findAll();
+    res.json(data);
+});
+
+router.post("/createUser", async(req, res) => {
+    let name = req.body.firstName;
+    let data = await db.user.create({ firstName: name });
+    res.json(data);
+});
 
 const User = db.user;
 const Answer = db.Answer;
