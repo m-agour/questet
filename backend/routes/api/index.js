0bdd7aa98c958b1c417ae4d0c781bd41a947f61b
m-agour <mo.aggour@gmail.com>
2022-03-20 20:26:30 +0200

    added login for user api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("./user");
 const examRouter = require("./exam");
+const authRouter = require("./auth");
 
 /* GET home page. */
 router.use("/user", userRouter);
 router.use("/exam", examRouter);
+router.use("/auth", authRouter);
 
 router.get("/", (req, res) => {
     res.status(200).json("<h1>API working!</h1>");
