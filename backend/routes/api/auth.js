257eaed281172413d5a16a85299e9238d2645536
m-agour <mo.aggour@gmail.com>
2022-03-20 19:27:15 +0200

    added login and optimized user route

---
+++ backend/routes/api/auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { dirname } = require("path");
+const userContoller = require(dirname(require.main.filename) +
+    "/controller/user.js");
+const db = require(dirname(require.main.filename) + "/models");
+
+const router = express.Router();
+
+/* GET home page. */
+router.post("/", userContoller.createUser);
+
+router.post("/createUser", async(req, res) => {
+    let name = req.body.firstName;
+    let data = await db.user.create({ firstName: name });
+    res.json(data);
+});
+
+const User = db.user;
+const Answer = db.Answer;
+const answer = db.answer;
+/* GET users listing. */
+router.get("/", async(req, res) => {
+    let data = await User.create({ name: "moahmed" });
+    res.json(data);
+});
+module.exports = router;
