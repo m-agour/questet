2bfd082b5d6584cb078de8b40e56e60d00254d91
mohamed alqblawi <mohammed30971488@f-eng.tanta.edu.eg>
2022-03-20 21:50:35 +0200

    create title and total point and public

--- backend/routes/api/exam.js
+++ backend/routes/api/exam.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { dirname } = require("path");
+const db = require(dirname(require.main.filename) + "/models");
 
-/* GET home page. */
-router.get("/testing", (req, res) => {});
+// create exam
+
+router.post("/createExam", async (req, res) => {
+  // 1- create title
+  let Title = await db.exam.create({ title: req.body.title });
+  // 2- total points
+  let CountPoints = await db.question.count({ points: req.body.points });
+  let TotalPoints = await db.exam.create({ totalPoints: CountPoints });
+  // 3-public
+  let PublicExam = await db.exam.create({ public: req.body.public });
+  // 4-Timed
+
+  res.json(Title);
+  res.json(TotalPoints);
+  res.json(PublicExam);
+});
+
+// Edit and update exam
+
+// Delete exam
 
 module.exports = router;
