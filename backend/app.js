e666b26e61a24ff2d71d1caf8585aefc00d17fc8
mohamed alqblawi <mohammed30971488@f-eng.tanta.edu.eg>
2022-04-08 23:34:50 +0200

    alqblawi

--- backend/app.js
+++ backend/app.js
@@ -37,5 +37,5 @@ app.use("/api", apiRouter);
 const port = process.env.PORT || "5000";
 
 app.listen(port, () => {
-    console.log(`Listen on ${port}`);
+  console.log(`Listen on ${port}`);
 });
--- backend/routes/api/exam.js
+++ backend/routes/api/exam.js
@@ -1,27 +1,9 @@
 const express = require("express");
-const router = express.Router();
 const { dirname } = require("path");
-const db = require(dirname(require.main.filename) + "/models");
+const examContoller = require(dirname(require.main.filename) +
+  "/controller/exam.js");
+const router = express.Router();
 
-// create exam
-
-router.post("/createExam", async (req, res) => {
-  // 1- create title
-  let Title = await db.exam.create({ title: req.body.title });
-  // 2- total points
-  let CountPoints = await db.question.count({ points: req.body.points });
-  let TotalPoints = await db.exam.create({ totalPoints: CountPoints });
-  // 3-public
-  let PublicExam = await db.exam.create({ public: req.body.public });
-  // 4-Timed
-
-  res.json(Title);
-  res.json(TotalPoints);
-  res.json(PublicExam);
-});
-
-// Edit and update exam
-
-// Delete exam
+router.post("/createExam", examContoller.createExam);
 
 module.exports = router;
